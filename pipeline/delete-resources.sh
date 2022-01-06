4f27863235a5c0f8ec53652e0abfd82764354717
Andreas Hahn <andreas.hahn@dataformers.at>
2022-01-06 11:23:58 +0100

    ad delete script

---
+++ pipeline/delete-resources.sh
@@ -0,0 +1,3 @@
+Remove-AzResourceGroup -Name surveillance-auto
+
+Remove-AzResource -ResourceId /subscriptions/548e97b4-f696-4666-bf7c-d15f03dfdcd5/providers/Microsoft.CognitiveServices/locations/westeurope/resourceGroups/surveillance-auto/deletedAccounts/surv-cognitive-services -ApiVersion 2021-04-30
